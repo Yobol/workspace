051806b9713088098b015b1dede672ad3e0b94f1
yobol <m15152036579@163.com>
2023-04-04 02:16:17 +0000

    linux - update github

--- README.md
+++
@@ -1,2 +0,0 @@
-# workspace
-A workspace to save and recovery personal development environment.
--- os/linux/ubuntu/v22.04-lts/git.sh
+++ os/linux/ubuntu/v22.04-lts/git.sh
@@ -4,15 +4,20 @@ sudo apt-get install -y git
 
 gh_account_name="yobol"
 gh_account_email="m15152036579@163.com"
+
+gh_ssh_crypt="ed25519"
+gh_ssh_prefix="id_${gh_ssh_crypt}_github"
 # GitHub
-git config --global user.name ${gh_account_name}
-git config --gloabal user.email ${gh_account_email}
+git config user.name ${gh_account_name}
+git config user.email ${gh_account_email}
 
 # # setup SSH keys associated with your Github account: 
 # https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
-ssh-keygen -t ed25519 -C ${gh_account_email}
+ssh-keygen -t ${gh_ssh_crypt} -C ${gh_account_email}
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
-cat ~/.ssh/id_ed25519.pub
+ssh-add ~/.ssh/${gh_ssh_prefix}
+cat ~/.ssh/${gh_ssh_prefix}.pub
 # copy and new SSH key: # https://github.com/settings/keys
 # TODO 添加交互逻辑：确认
+
+git remote set-url origin git@github.com:Yobol/workspace.git
